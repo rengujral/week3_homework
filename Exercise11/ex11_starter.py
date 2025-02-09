63919322357c129fed55333c6a87544af3698825
Renee Gujral <reneegujral1@gmail.com>
2025-02-09 15:59:27 +0000

    EDIT: added comments to file

--- Exercise11/ex11_starter.py
+++ Exercise11/ex11_starter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # pre-existing string taken from example provided with details about Belgium
 # print variable
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
@@ -27,7 +29,7 @@ print(":".join(Belgium_list))
 # finding the total population of Belgium and its capital
 # population of Belgium is the second substring, thus [1] (python counts up from 0)
 # population of Brussels is the fourth substring, thus [3]
-# int function converts values from strings into integers before finding total 
+# int function converts values from strings into integers before finding total
 # print final value of total_popu variable
 total_popu = int(Belgium_list[1]) + int(Belgium_list[3])
 print(total_popu)
